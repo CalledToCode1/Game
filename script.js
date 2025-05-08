04e123a856a58dc831babff14a139a4fb8768d32
JAVAriable <152537522+CalledToCode1@users.noreply.github.com>
2025-05-08 15:35:47 -0400

    Canvas size change, as well as player location fix. Allows for player movement using wasd keys.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 window.addEventListener("DOMContentLoaded", function () {
-    var c = document.getElementById("myCanvas");
+    var c = document.getElementById("backgroundCanvas");
 
     if (c && c.getContext) {
         var ctx = c.getContext("2d");
@@ -17,19 +17,46 @@ window.addEventListener("DOMContentLoaded", function () {
     }
 });
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "blue";
-    ctx.fillRect(x, y, size, size);
+
+const bgCanvas = document.getElementById("backgroundCanvas");
+const playerCanvas = document.getElementById("foregroundCanvas");
+const bgCtx = bgCanvas.getContext("2d");
+const fgCtx = playerCanvas.getContext("2d");
+
+let playerX = 150;
+let playerY = 150;
+const playerWidth = 50;
+const playerHeight = 50;
+const playerSpeed = 5;
+
+function drawBackground() {
+    bgCtx.fillStyle = "white";
+    bgCtx.fillRect(0, 0, bgCanvas.width, bgCanvas.height);
 }
 
-window.addEventListener("keydown", function(e)  {
-    switch (e.key) {
+function drawPlayer()
+{
+    fgCtx.clearRect(0, 0, playerCanvas.width, playerCanvas.height);
+    fgCtx.fillStyle = "light blue";
+    fgCtx.fillRect(playerX, playerY, playerWidth, playerHeight);
+}
+
+window.addEventListener("keydown", (e) => {
+    switch(e.key) {
         case "w":
-        
-
+            if (playerY > 0) playerY -= playerSpeed;
+            break;
+        case "s":
+            if (playerY < bgCanvas.height - playerHeight) playerY += playerSpeed;
+            break;
+        case "a":
+            if (playerX > 0) playerX -= playerSpeed;
+            break;
+        case "d":
+            if (playerX < bgCanvas.width - playerWidth) playerY += playerSpeed;
+            break;
     }
-        
+
+    drawBackground();
+    drawPlayer();
 });
-
-
