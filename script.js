2b843f51f3cedd9c766e88a7b31d065d4fa5734f
JAVAriable <152537522+CalledToCode1@users.noreply.github.com>
2025-05-08 14:47:48 -0400

    Draw Test

--- script.js
+++ script.js
@@ -9,12 +9,27 @@ window.addEventListener("DOMContentLoaded", function () {
         ctx.moveTo(0, 0);
         ctx.lineTo(400, 200);
         ctx.stroke();
-        
+
     } else {
 
         console.error("Canvas is not working");
 
     }
+});
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "blue";
+    ctx.fillRect(x, y, size, size);
+}
+
+window.addEventListener("keydown", function(e)  {
+    switch (e.key) {
+        case "w":
+        
+
+    }
+        
 });
 
 
