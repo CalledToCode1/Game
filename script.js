041852066f7e666900981268c1547b4365b9fb8a
JAVAriable <152537522+CalledToCode1@users.noreply.github.com>
2025-05-08 15:36:47 -0400

    Removing line from the center of the Canvas

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
 
-window.addEventListener("DOMContentLoaded", function () {
-    var c = document.getElementById("backgroundCanvas");
 
-    if (c && c.getContext) {
-        var ctx = c.getContext("2d");
-
-
-        ctx.moveTo(0, 0);
-        ctx.lineTo(400, 200);
-        ctx.stroke();
-
-    } else {
-
-        console.error("Canvas is not working");
-
-    }
-});
 
 
 const bgCanvas = document.getElementById("backgroundCanvas");
