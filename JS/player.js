67210f04b6073558c401423db1673d0eebd3788a
JAVAriable <152537522+CalledToCode1@users.noreply.github.com>
2025-05-11 17:59:46 -0400

    Player movement hopefully = ] also larger canvas

--- JS/player.js
+++ JS/player.js
@@ -1,11 +1,32 @@
 function Player(x, y){ //a constructor (i think)
     this.x = x;
     this.y = y;
+    this.xSpeed = 0;
+    this.ySpeed = 0;
+    this.friction = 0.6;
+    this.maxSpeed = 10;
     this.width = 50;
     this.height = 100;
+    this.active = true;
 
     this.step = function(){
+        //Movement
+        if (this.active) {
+            //Horizontal Movement
+            if (!leftKey && !rightKey || leftKey && rightKey) {
+                //Slow Down
+                this.xspeed *= this.friction; // why does it multiply? //friction coefficient ig i dunno
+            } else if (rightKey ){
+                //Move Right
+                this.xSpeed++;
+            } else if (leftKey) {
+                this.ySpeed--;
+            }
+            //Vertical Movement
 
+            //Correct Speed
+            this.x += this.xSpeed;
+        }
     }
 
     this.draw = function(){
--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,21 @@
 var canvas;
 var ctx;
 
+//input variables
+var upKey;
+var rightKey;
+var downKey;
+var leftKey;
+
 var gameLoop;
 var player;
 
 window.onload = function() {
     canvas = document.getElementById("myCanvas"); //make sure this works ============
     ctx = canvas.getContext("2d");
+
+    //inputs set up
+    setupInputs();
 
     player = new Player(50, 200); //created player
 
@@ -30,4 +39,30 @@ function draw() {
 
     //draw player
     player.draw();
+}
+
+function setupInputs() {
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "w" || event.key === "ArrowUp") {
+            upKey = true;
+        } else if (event.key === "a" || event.key === "ArrowLeft") {
+            leftKey = true;
+        } else if (event.key === "s" || event.key === "ArrowRight") {
+            rightKey = true;
+        } else if (event.key === "d" || event.key === "ArrowDown") {
+            downKey = true;
+        }
+    });
+
+    document.addEventListener("keyUp", function(event) {
+        if (event.key === "w" || event.key === "ArrowUp") {
+            upKey = false;
+        } else if (event.key === "a" || event.key === "ArrowLeft") {
+            leftKey = false;
+        } else if (event.key === "s" || event.key === "ArrowRight") {
+            rightKey = false;
+        } else if (event.key === "d" || event.key === "ArrowDown") {
+            downKey = false;
+        }
+    });
 }
