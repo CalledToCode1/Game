691a39a2bf0c612fd1adb4f514b9d9f92c1eb5d7
JAVAriable <152537522+CalledToCode1@users.noreply.github.com>
2025-05-12 15:00:31 -0400

    Added border

---
+++ JS/border.js
@@ -0,0 +1,19 @@
+function Border(x, y, width, height, type) {
+    this.x = x;
+    this.y = y;
+
+    this.width = width;
+    this.height = height;
+    
+    this.type = type; // type of border that it is
+
+    this.draw = function() {
+        if (this.type === 1) {
+            ctx.fillStyle = "blue";
+            
+        } else if (this.type === 2) {
+            ctx.fillStyle = "red";
+        }
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
